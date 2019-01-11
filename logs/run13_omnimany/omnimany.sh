7cbbc9f897760b33b42a8946bfdcc8d50b19987b
Ubuntu <ubuntu@ip-172-31-30-144.us-east-2.compute.internal>
2019-01-11 06:08:17 +0000

    omnimany

---
+++ logs/run13_omnimany/omnimany.sh
@@ -0,0 +1,3 @@
+for weight in 0.1 1 10 ; do
+    python -u run_omniglot.py --shots 1 --inner-batch 10 --inner-iters 25 --meta-step 1 --meta-batch 5 --meta-iters 25000 --eval-batch 5 --eval-iters 50 --learning-rate 0.001 --meta-step-final 0 --train-shots 10 --checkpoint ckpt_omnimany$weight --transductive --eval-samples 1000 --ftrl $weight 2>&1 | tee omnimany$weight.log
+done
