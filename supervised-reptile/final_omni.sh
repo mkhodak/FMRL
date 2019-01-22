c96ace737a9adfdaa0aa07e586e1dcd110d53301
Ubuntu <ubuntu@ip-172-31-70-115.ec2.internal>
2019-01-22 22:33:04 +0000

    final

--- supervised-reptile/final_omni.sh
+++ supervised-reptile/final_omni.sh
@@ -1,7 +1,7 @@
-python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 5 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 200 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_rept --eval-samples 500 2>&1 | tee omni_rept.log
+python -u final_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 5 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 5000 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_rept --eval-samples 500 --eval-interval 250 2>&1 | tee omni_rept.log
 
-python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 5 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 200 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_ftrl --eval-samples 500 --ftrl 0.01 2>&1 | tee omni_ftrl.log
+python -u final_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 5 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 5000 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_ftrl --eval-samples 500 --eval-interval 250 --ftrl 1.0 2>&1 | tee omni_ftrl.log
 
-python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 25 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 200 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_many --eval-samples 500 2>&1 | tee omni_many.log
+python -u final_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 25 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 5000 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_many --eval-samples 500 --eval-interval 250 2>&1 | tee omni_many.log
 
-python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 25 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 200 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_both --eval-samples 500 --ftrl 0.01 2>&1 | tee omni_both.log
+python -u final_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 25 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 5000 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_both --eval-samples 500 --eval-interval 250 --ftrl 1.0 2>&1 | tee omni_both.log
