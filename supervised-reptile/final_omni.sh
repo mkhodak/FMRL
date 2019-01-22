7824c408c3a453740f0f7baf0f9a11ff69a508bd
Ubuntu <ubuntu@ip-172-31-70-115.ec2.internal>
2019-01-22 21:27:26 +0000

    smaller ftrl

--- supervised-reptile/final_omni.sh
+++ supervised-reptile/final_omni.sh
@@ -4,4 +4,4 @@ python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 5 --l
 
 python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 25 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 200 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_many --eval-samples 500 2>&1 | tee omni_many.log
 
-python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 25 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 200 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_both --eval-samples 500 --ftrl 1.0 2>&1 | tee omni_both.log
+python -u eval_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 25 --learning-rate 0.001 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 200 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni_both --eval-samples 500 --ftrl 0.01 2>&1 | tee omni_both.log
