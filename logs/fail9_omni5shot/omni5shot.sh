e8c91944a0edbf2f13c77ee1bdb9bf3a6932b9bd
mkhodak <khodak@cmu.edu>
2019-01-30 22:58:59 -0500

    rm

--- logs/fail9_omni5shot/omni5shot.sh
+++
@@ -1,3 +0,0 @@
-python -u run_omniglot.py --train-shots 10 --inner-batch 10 --inner-iters 5 --learning-rate 0.01 --meta-step 1 --meta-step-final 0 --meta-batch 5 --meta-iters 100000 --eval-batch 5 --eval-iters 50 --checkpoint ckpt_omni55 --transductive --ftrl 1.0 --sgd 2>&1 | tee omni55.log
-
-python -u run_omniglot.py --classes 20 --inner-batch 20 --inner-iters 10 --meta-step 1 --meta-batch 5 --meta-iters 200000 --eval-batch 10 --eval-iters 50 --learning-rate 0.005 --meta-step-final 0 --train-shots 10 --checkpoint ckpt_omni520 --transductive --ftrl 1.0 --sgd 2>&1 | tee omni520.log
--- supervised-reptile/final_mini.sh
+++
@@ -1,7 +0,0 @@
-python -u final_miniimagenet.py --inner-batch 10 --inner-iters 8 --meta-step 1 --meta-batch 5 --meta-iters 5001 --eval-batch 15 --eval-iters 50 --learning-rate 0.001 --meta-step-final 0 --train-shots 15 --eval-samples 500 --eval-interval 250 --checkpoint ckpt_mini_rept 2>&1 | tee mini_rept.log
-
-python -u final_miniimagenet.py --inner-batch 10 --inner-iters 8 --meta-step 1 --meta-batch 5 --meta-iters 5001 --eval-batch 15 --eval-iters 50 --learning-rate 0.001 --meta-step-final 0 --train-shots 15 --eval-samples 500 --eval-interval 250 --ftrl 1.0 --checkpoint ckpt_mini_ftrl 2>&1 | tee mini_ftrl.log
-
-python -u final_miniimagenet.py --inner-batch 10 --inner-iters 40 --meta-step 1 --meta-batch 5 --meta-iters 5001 --eval-batch 15 --eval-iters 50 --learning-rate 0.001 --meta-step-final 0 --train-shots 15 --eval-samples 500 --eval-interval 250 --checkpoint ckpt_mini_many 2>&1 | tee mini_many.log
-
-python -u final_miniimagenet.py --inner-batch 10 --inner-iters 40 --meta-step 1 --meta-batch 5 --meta-iters 5001 --eval-batch 15 --eval-iters 50 --learning-rate 0.001 --meta-step-final 0 --train-shots 15 --eval-samples 500 --eval-interval 250 --ftrl 1.0 --checkpoint ckpt_mini_both 2>&1 | tee mini_both.log
